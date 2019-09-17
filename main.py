3b5027cff728c2eb2ed8db76d320c1ded02e057c
praveen098 <31249049+praveen098@users.noreply.github.com>
2019-09-17 14:37:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,65 +11,30 @@ sns.set_style('darkgrid')
 pd.options.display.float_format = '{:,.3f}'.format
 
 
-# ## Load data
-
-# In[2]:
-
-
 get_ipython().run_cell_magic('time', '', "\nparser = lambda date: pd.to_datetime(date, format='%d.%m.%Y')\n\ntrain = pd.read_csv('../input/sales_train.csv', parse_dates=['date'], date_parser=parser)\ntest = pd.read_csv('../input/test.csv')\nitems = pd.read_csv('../input/items.csv')\nitem_cats = pd.read_csv('../input/item_categories.csv')\nshops = pd.read_csv('../input/shops.csv')\n\nprint('train:', train.shape, 'test:', test.shape)\nprint('items:', items.shape, 'item_cats:', item_cats.shape, 'shops:', shops.shape)")
 
-
-# In[3]:
-
-
 train.head()
-
-
-# In[4]:
 
 
 test.head()
 
 
-# In[5]:
-
-
 items.head()
-
-
-# In[6]:
 
 
 item_cats.head()
 
 
-# In[7]:
-
-
 shops.head()
-
-
-# ## Data analysis
-
-# In[8]:
 
 
 print(train['date_block_num'].max())
 
 
-# In[9]:
-
-
 print(train['item_cnt_day'].describe())
 
 
-# In[10]:
-
-
 train['item_cnt_day'].nlargest(25).values
-
-
-# In[11]:
 
 
 test_only = test[~test['item_id'].isin(train['item_id'].unique())]['item_id'].unique()
@@ -81,19 +43,13 @@ print('test only items:', len(test_only))
 
 # ## Preprocessing
 
-# In[12]:
 
-
-# drop duplicates
 subset = ['date','date_block_num','shop_id','item_id','item_cnt_day']
 print(train.duplicated(subset=subset).value_counts())
 train.drop_duplicates(subset=subset, inplace=True)
 
 
-# In[13]:
 
-
-# drop shops&items not in test data
 test_shops = test.shop_id.unique()
 test_items = test.item_id.unique()
 train = train[train.shop_id.isin(test_shops)]
@@ -102,12 +58,10 @@ train = train[train.item_id.isin(test_items)]
 print('train:', train.shape)
 
 
-# In[14]:
 
 
 from itertools import product
 
-# create all combinations
 block_shop_combi = pd.DataFrame(list(product(np.arange(34), test_shops)), columns=['date_block_num','shop_id'])
 shop_item_combi = pd.DataFrame(list(product(test_shops, test_items)), columns=['shop_id','item_id'])
 all_combi = pd.merge(block_shop_combi, shop_item_combi, on=['shop_id'], how='inner')
@@ -119,12 +73,7 @@ train_base['item_cnt_day'].fillna(0, inplace=True)
 train_grp = train_base.groupby(['date_block_num','shop_id','item_id'])
 
 
-# ## Aggregate
 
-# In[15]:
-
-
-# summary count by month
 train_monthly = pd.DataFrame(train_grp.agg({'item_cnt_day':['sum','count']})).reset_index()
 train_monthly.columns = ['date_block_num','shop_id','item_id','item_cnt','item_order']
 print(train_monthly[['item_cnt','item_order']].describe())
@@ -135,9 +84,7 @@ train_monthly['item_cnt'].clip(0, 20, inplace=True)
 train_monthly.head()
 
 
-# ### Feature creation
 
-# In[16]:
 
 
 # pickup first category name
@@ -148,8 +95,6 @@ items = pd.merge(items, item_cats.loc[:,['item_category_id','item_group']], on=[
 
 item_grp.unique()
 
-
-# In[17]:
 
 
 city = shops.shop_name.apply(lambda x: str.replace(x, '!', '')).apply(lambda x: x.split(' ')[0])
@@ -327,103 +272,4 @@ test_price_a['discount_rate'] = 1 - (test_price_a['item_price'] / test_price_a['
 test_price_a.drop('item_max_price', axis=1, inplace=True)
 test_price_a.head()
 
-
-# ## Data preparation
-
-# In[30]:
-
-
-# Memory saving function credit to https://www.kaggle.com/gemartin/load-data-reduce-memory-usage
-def reduce_mem_usage(df):
-    """ iterate through all the columns of a dataframe and modify the data type
-        to reduce memory usage.
-    """
-    start_mem = df.memory_usage().sum() / 1024**2
-    
-    for col in df.columns:
-        col_type = df[col].dtype
-        
-        if col_type != object:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)  
-            else:
-                #if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-                #    df[col] = df[col].astype(np.float16)
-                #el
-                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
-        #else:
-            #df[col] = df[col].astype('category')
-
-    end_mem = df.memory_usage().sum() / 1024**2
-    print('Memory usage of dataframe is {:.2f} MB --> {:.2f} MB (Decreased by {:.1f}%)'.format(
-        start_mem, end_mem, 100 * (start_mem - end_mem) / start_mem))
-    return df
-
-
-# In[31]:
-
-
-def mergeFeature(df): 
-  df = pd.merge(df, items, on=['item_id'], how='left').drop('item_group', axis=1)
-  df = pd.merge(df, item_cats, on=['item_category_id'], how='left')
-  df = pd.merge(df, shops, on=['shop_id'], how='left')
-
-  df = pd.merge(df, train_shop, on=['shop_id','item_id'], how='left')
-  df = pd.merge(df, train_shop_cat, on=['shop_id','item_group'], how='left')
-  df = pd.merge(df, train_prev, on=['date_block_num','shop_id','item_id'], how='left')
-  df = pd.merge(df, train_cat_prev, on=['date_block_num','shop_id','item_group'], how='left')
-  df = pd.merge(df, train_ema_prev, on=['date_block_num','shop_id','item_id'], how='left')
-  
-  df['month'] = df['date_block_num'] % 12
-  days = pd.Series([31,28,31,30,31,30,31,31,30,31,30,31])
-  df['days'] = df['month'].map(days).astype(np.int8)
-  
-  df.drop(['shop_id','shop_name','item_id','item_name','item_category_id','item_category_name','item_group'], axis=1, inplace=True)
-  df.fillna(0.0, inplace=True)
-  return reduce_mem_usage(df)
-
-
-# In[32]:
-
-
-train_set = train_monthly[train_monthly['date_block_num'] >= 12]
-
-train_set = pd.merge(train_set, train_price_a, on=['date_block_num','shop_id','item_id'], how='left')
-train_set = mergeFeature(train_set)
-
-train_set = train_set.join(pd.DataFrame(train_set.pop('item_order'))) # move to last column
-X_train = train_set.drop(['item_cnt'], axis=1)
-#Y_train = train_set['item_cnt']
-Y_train = train_set['item_cnt'].clip(0.,20.)
-X_train.head()
-
-
-# In[33]:
-
-
-test_set = test.copy()
-test_set['date_block_num'] = 34
-
-test_set = pd.merge(test_set, test_price_a, on=['shop_id','item_id'], how='left')
-test_set = mergeFeature(test_set)
-
-test_set['item_order'] = test_set['cnt_ema_s_prev'] #order_prev
-test_set.loc[test_set['item_order'] == 0, 'item_order'] = 1
-
-X_test = test_set.drop(['ID'], axis=1)
-X_test.head()
-
-assert(X_train.columns.isin(X_test.columns).all())
 
